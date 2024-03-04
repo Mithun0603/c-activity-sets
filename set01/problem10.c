1f399ca268775ebc9837e0fe542493ff070df986
Mithun R <mithunr.bsc23@rvu.edu.in>
2024-03-04 14:37:14 +0530

    test

---
+++ set01/problem10.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void) {
+
+int i;
+char ch1[100],ch2[100];
+
+  printf("enter the first string:\n");
+scanf("%s",&ch1);
+
+  printf("enter the second string:\n");
+  scanf("%s",&ch2);
+
+
+for(int i=0 ;ch1[i] == ch2[i]&&ch1[i]!='\0'&&ch2[i]!='\0';i++);
+
+if(ch1[i]>ch2[i])
+{
+printf("%s is greater\n",ch1);
+  return 0;
+  
+}
+
+  if(ch2[i]>ch1[i])
+  {
+  printf("%s is greater\n",ch1);
+    return 0;
+
+  }
+
+  else
+  {
+    printf("strings are equal\n");
+  }
+return 0;
+}
